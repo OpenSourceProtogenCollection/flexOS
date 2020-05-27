b18d12642639628ea1029705229b66972c919e56
Jared Tamana <seanythedream@gmail.com>
2020-05-27 19:28:56 -0400

    Add second USB

--- raspberry-pi/main.py
+++ raspberry-pi/main.py
@@ -42,6 +42,7 @@ GPIO.add_event_detect(laughPin, GPIO.BOTH, callback=writeLaugh, bouncetime=800)
 GPIO.add_event_detect(owoPin, GPIO.BOTH, callback=writeOwo, bouncetime=800)
 GPIO.add_event_detect(sleepPin, GPIO.BOTH, callback=writeSleep, bouncetime=800)
 
+# USB declarations begin here
 visorEyes = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 visorMouth = serial.Serial('dev/ttyUSB1', 9600, timeout=1)
 visorEyes.flush()
@@ -51,63 +52,84 @@ visorMouth.flush()
 
 def writeAttn(): # might not include the mouth, not sure
     visorEyes.write(b"1") # priority 4, left thumb
+	visorMouth.write(b"1")
 
 def writeChg():
     visorEyes.write(b"2") # priority 4, left index
+	visorMouth.write(b"2")
 
 def writeLow():
     visorEyes.write(b"g") # priority 5, left middle
+	visorMouth.write(b"g")
 
 def writeLost():
     visorEyes.write(b"f") # priority 5, left ring
+	visorMouth.write(b"f")
 
 def writeDni():
     visorEyes.write(b"4") # priority 5, left small
+	visorMouth.write(b"4")
 
 def writeIdle():
     visorEyes.write(b"8") # priority 5, right thumb
+	visorMouth.write(b"8")
     
 def writeCheck():
     visorEyes.write(b"6") # priority 4, right index
+	visorMouth.write(b"6")
 
 def writeWrong():
     visorEyes.write(b"7") # priority 4, right middle
+	visorMouth.write(b"7")
 
 def writeId():
     visorEyes.write(b"0") # priority 4, right ring
+	visorMouth.write(b"0")
 
 def writeJoy():
     visorEyes.write(b"a") # priority 4, right small
+	visorMouth.write(b"a")
 
 def writeLoad():
     visorEyes.write(b"e") # priority 3, left collar
+	visorMouth.write(b"e")
     
 def writeHungry():
     visorEyes.write(b"k") # priority 3, left beltline
+	visorMouth.write(b"k")
 
 def writeError():
     visorEyes.write(b"5") # priority 3, right beltline
+	visorMouth.write(b"5")
 
 def writeLaugh():
     visorEyes.write(b"b") # priority 3, left hip
+	visorMouth.write(b"b")
 
 def writeSleep():
     visorEyes.write(b"d") # priority 3, right hip
+	visorMouth.write(b"d")
 
 def writeOwo():
     visorEyes.write(b"h") # priority 3, right collar
+	visorMouth.write(b"h")
     
 def writeHeart():
     visorEyes.write(b"j") # priority 2
+	visorMouth.write(b"j")
 
 def writeCoffee():
     visorEyes.write(b"3") # priority 1
+	visorMouth.write(b"3")
 
 def writeUwu():
     visorEyes.write(b"i") # priority 1
+	visorMouth.write(b"i")
 
 def writePhone():
     visorEyes.write(b"c") # priority 1
+	visorMouth.write(b"c")
 
 def writeCry():
     visorEyes.write(b"9")
+	visorMouth.write(b"9")
