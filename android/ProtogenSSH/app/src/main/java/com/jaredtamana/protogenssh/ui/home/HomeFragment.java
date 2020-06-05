4efc1ef277fad9d9456b333d2bd162e9d0dbd7ed
Jared Tamana <seanythedream@gmail.com>
2020-06-04 22:30:43 -0400

    Lots of fixes :3

--- android/ProtogenSSH/app/src/main/java/com/jaredtamana/protogenssh/ui/home/HomeFragment.java
+++ android/ProtogenSSH/app/src/main/java/com/jaredtamana/protogenssh/ui/home/HomeFragment.java
@@ -7,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.Toast;
@@ -18,12 +17,10 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 // Project imports
+import com.google.android.material.textfield.TextInputLayout;
 import com.jaredtamana.protogenssh.R;
-import com.jaredtamana.protogenssh.ui.eyes.EyesFragment;
-import com.jaredtamana.protogenssh.ui.mouth.MouthFragment;
 import com.jaredtamana.protogenssh.ui.Functions;
 
-@SuppressWarnings("ALL")
 public class HomeFragment extends Fragment { // main fragment start
 
     private HomeViewModel homeViewModel;
@@ -35,14 +32,14 @@ public class HomeFragment extends Fragment { // main fragment start
         final View root = inflater.inflate(R.layout.fragment_home, container, false);
         Button fabCreateScript = (Button) root.findViewById(R.id.fab);
         final LinearLayout mEmoteList = (LinearLayout) root.findViewById(R.id.scrollViewEmoteListLayout);
-        Functions.readFile(mEmoteList, "buttonsMouthOnly.txt", getContext(), getActivity());
+        Functions.readFile(mEmoteList, "buttonsFullFace.txt", getContext(), getActivity());
         fabCreateScript.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
                 View mView = getLayoutInflater().inflate(R.layout.dialog_add_script, null);
-                final EditText mScriptName = (EditText) mView.findViewById(R.id.scriptName);
-                final EditText mScriptPath = (EditText) mView.findViewById(R.id.scriptPath);
+                final TextInputLayout mScriptName = (TextInputLayout) mView.findViewById(R.id.scriptName);
+                final TextInputLayout mScriptPath = (TextInputLayout) mView.findViewById(R.id.scriptPath);
                 final RadioButton mRadioFullFace = (RadioButton) mView.findViewById(R.id.radioFullFace);
                 final RadioButton mRadioEyesOnly = (RadioButton) mView.findViewById(R.id.radioEyesOnly);
                 final RadioButton mRadioMouthOnly = (RadioButton) mView.findViewById(R.id.radioMouthOnly);
@@ -60,29 +57,29 @@ public class HomeFragment extends Fragment { // main fragment start
                 mBtnScriptApply.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        if (mScriptName.getText().toString().trim().equals("")) { // WHY IS THIS SO GROSS
+                        if (mScriptName.getEditText().getText().toString().trim().equals("")) { // WHY IS THIS SO GROSS
                             mScriptName.setError("Please add a name for this script.");
                             return;
-                        } else if (mScriptPath.getText().toString().trim().equals("")) { // GOD
+                        } else if (mScriptPath.getEditText().getText().toString().trim().equals("")) { // GOD
                             mScriptPath.setError("Please add a valid command for this script.");
                             return;
                         }
                         Button nb = new Button(getActivity());
-                        nb.setText(mScriptName.getText().toString());
+                        nb.setText(mScriptName.getEditText().getText().toString());
                         nb.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
-                                Functions.executeSSHcommand(mScriptPath.getText().toString(), getContext());
+                                Functions.executeSSHcommand(mScriptPath.getEditText().getText().toString(), getContext());
                             }
                         });
                         try {
                             if (mRadioFullFace.isChecked()) {
                                 mEmoteList.addView(nb);
-                                Functions.writeFile(mScriptName.getText().toString(), mScriptPath.getText().toString(), "buttonsFullFace.txt", getContext());
+                                Functions.writeFile(mScriptName.getEditText().getText().toString(), mScriptPath.getEditText().getText().toString(), "buttonsFullFace.txt", getContext());
                             } else if (mRadioEyesOnly.isChecked()) {
-                                Functions.writeFile(mScriptName.getText().toString(), mScriptPath.getText().toString(), "buttonsEyesOnly.txt", getContext());
+                                Functions.writeFile(mScriptName.getEditText().getText().toString(), mScriptPath.getEditText().getText().toString(), "buttonsEyesOnly.txt", getContext());
                             } else if (mRadioMouthOnly.isChecked()) {
-                                Functions.writeFile(mScriptName.getText().toString(), mScriptPath.getText().toString(), "buttonsMouthOnly.txt", getContext());
+                                Functions.writeFile(mScriptName.getEditText().getText().toString(), mScriptPath.getEditText().getText().toString(), "buttonsMouthOnly.txt", getContext());
                             } else {
                                 Toast chooseCategoryToast = Toast.makeText(getContext(), "Please choose a category for this command.", Toast.LENGTH_SHORT);
                                 chooseCategoryToast.setMargin(50, 50);
--- android/ProtogenSSH/app/src/main/java/com/jaredtamana/protogenssh/ui/mouth/MouthFragment.java
+++ android/ProtogenSSH/app/src/main/java/com/jaredtamana/protogenssh/ui/mouth/MouthFragment.java
@@ -6,40 +6,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.jaredtamana.protogenssh.R;
 import com.jaredtamana.protogenssh.ui.Functions;
 
-public class MouthFragment extends Fragment {
+public class MouthFragment extends Fragment { // main fragment start
 
-    private com.jaredtamana.protogenssh.ui.mouth.MouthViewModel eyesViewModel;
+    private MouthViewModel eyesViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         eyesViewModel =
-                ViewModelProviders.of(this).get(com.jaredtamana.protogenssh.ui.mouth.MouthViewModel.class);
+                ViewModelProviders.of(this).get(MouthViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         Button fabCreateScript = (Button) root.findViewById(R.id.fab);
         final LinearLayout mEmoteList = (LinearLayout) root.findViewById(R.id.scrollViewEmoteListLayout);
-        Functions.readFile(mEmoteList, "buttonsFullFace.txt", getContext(), getActivity());
+        Functions.readFile(mEmoteList, "buttonsMouthOnly.txt", getContext(), getActivity());
         fabCreateScript.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
                 View mView = getLayoutInflater().inflate(R.layout.dialog_add_script, null);
-                final EditText mScriptName = (EditText) mView.findViewById(R.id.scriptName);
-                final EditText mScriptPath = (EditText) mView.findViewById(R.id.scriptPath);
+                final TextInputLayout mScriptName = (TextInputLayout) mView.findViewById(R.id.scriptName);
+                final TextInputLayout mScriptPath = (TextInputLayout) mView.findViewById(R.id.scriptPath);
                 final RadioButton mRadioFullFace = (RadioButton) mView.findViewById(R.id.radioFullFace);
                 final RadioButton mRadioEyesOnly = (RadioButton) mView.findViewById(R.id.radioEyesOnly);
                 final RadioButton mRadioMouthOnly = (RadioButton) mView.findViewById(R.id.radioMouthOnly);
@@ -57,29 +54,34 @@ public class MouthFragment extends Fragment {
                 mBtnScriptApply.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        if (mScriptName.getText().toString().trim().equals("")) { // WHY IS THIS SO GROSS
+                        if (mScriptName.getEditText().getText().toString().trim().equals("")) { // WHY IS THIS SO GROSS
                             mScriptName.setError("Please add a name for this script.");
-                        } else if (mScriptPath.getText().toString().trim().equals("")) { // GOD
+                            return;
+                        } else if (mScriptPath.getEditText().getText().toString().trim().equals("")) { // GOD
                             mScriptPath.setError("Please add a valid command for this script.");
+                            return;
                         }
                         Button nb = new Button(getActivity());
-                        nb.setText(mScriptName.getText().toString());
+                        nb.setText(mScriptName.getEditText().getText().toString());
                         nb.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
-                                Functions.executeSSHcommand(mScriptPath.getText().toString(), getContext());
+                                Functions.executeSSHcommand(mScriptPath.getEditText().getText().toString(), getContext());
                             }
                         });
                         try {
                             if (mRadioFullFace.isChecked()) {
-                                mEmoteList.addView(nb);
-                                Functions.writeFile(mScriptName.getText().toString(), mScriptPath.getText().toString(), "buttonsFullFace.txt", getContext());
+                                Functions.writeFile(mScriptName.getEditText().getText().toString(), mScriptPath.getEditText().getText().toString(), "buttonsFullFace.txt", getContext());
                             } else if (mRadioEyesOnly.isChecked()) {
-                                Functions.writeFile(mScriptName.getText().toString(), mScriptPath.getText().toString(), "buttonsEyesOnly.txt", getContext());
+                                Functions.writeFile(mScriptName.getEditText().getText().toString(), mScriptPath.getEditText().getText().toString(), "buttonsEyesOnly.txt", getContext());
                             } else if (mRadioMouthOnly.isChecked()) {
-                                Functions.writeFile(mScriptName.getText().toString(), mScriptPath.getText().toString(), "buttonsMouthOnly.txt", getContext());
+                                mEmoteList.addView(nb);
+                                Functions.writeFile(mScriptName.getEditText().getText().toString(), mScriptPath.getEditText().getText().toString(), "buttonsMouthOnly.txt", getContext());
                             } else {
                                 Toast chooseCategoryToast = Toast.makeText(getContext(), "Please choose a category for this command.", Toast.LENGTH_SHORT);
+                                chooseCategoryToast.setMargin(50, 50);
+                                chooseCategoryToast.show();
+                                return;
                             }
                         } catch (NullPointerException ee) {
                             ee.printStackTrace();
