777bfd14b9c5d25f6689250575db1e386d6cebed
Jared Tamana <seanythedream@gmail.com>
2020-06-05 13:59:55 -0400

    Add Settings to nav drawer

--- android/ProtogenSSH/app/src/main/java/com/jaredtamana/protogenssh/MainActivity.java
+++ android/ProtogenSSH/app/src/main/java/com/jaredtamana/protogenssh/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_eyes, R.id.nav_mouth)
+                R.id.nav_home, R.id.nav_eyes, R.id.nav_mouth, R.id.nav_settings)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        // getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
