a4e16761cf34432197f9d97357ddac0f0f5aa234
Jared Tamana <seanythedream@gmail.com>
2020-06-10 14:34:24 -0400

    Add validation for user input (closes #11)

--- raspberry-pi/setup.sh
+++ raspberry-pi/setup.sh
@@ -12,12 +12,22 @@ sudo netfilter-persistent save
 sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
 sudo echo -e "interface=wlan0 # Listening interface\ndhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h # Pool of IP addresses served via DHCP\ndomain=wlan     # Local wireless DNS domain\naddress=/proto.gen/192.168.4.1 # Alias for this router" | sudo tee /etc/dnsmasq.conf
 sudo rfkill unblock wlan
+while [ ${#countrycode} != 2 ]
+do
 echo "Please enter your wifi country code to enable your WiFi spectrum (see https://preview.tinyurl.com/WiFiCodes): " 
 read countrycode
-echo "Please enter your desired network name: " 
+countrycode=${countrycode^^}
+done
+while [ ${#SSID} -lt 2 ] || [ ${#SSID} -gt 32 ]
+do
+echo "Please enter your desired network name (2-32 characters): " 
 read SSID
-echo "Please enter a password for the network: " 
+done
+while [ ${#netPass} -lt 8 ] || [ ${#netPass} -gt 63 ]
+do
+echo "Please enter a password for the network (8-63 characters): " 
 read netPass
+done
 echo -e "country_code=$countrycode\ninterface=wlan0\nssid=$SSID\nhw_mode=g\nchannel=7\nmacaddr_acl=0\nauth_algs=1\nignore_broadcast_ssid=0\nwpa=2\nwpa_passphrase=$netPass\nwpa_key_mgmt=WPA-PSK\nwpa_pairwise=TKIP\nrsn_pairwise=CCMP" | sudo tee -a /etc/hostapd/hostapd.conf
 echo "Your RasPi will reboot in 10 seconds to enable this configuration. To cancel reboot press CTRL+C"
 echo "After reboot you can access this device at 192.168.4.1, or at proto.gen on some devices"
